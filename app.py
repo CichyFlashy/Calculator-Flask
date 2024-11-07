a96f4aad099dd1e587500e004028ec98550239cb
Mateusz Cichos <mateusz.cichos@edu.uekat.pl>
2024-11-07 23:30:49 +0100

    Add function for subpage calculator

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-app.route('/')
+@app.route('/')
 def index():
-    return 'Hello world!'
+    return render_template('index.html')
+
+@app.route('/calculator')
+def calculator():
+    return render_template('calculator.html')
+
 
 if __name__ == "__main__":
     app.run()
