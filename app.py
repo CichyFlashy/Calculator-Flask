6e6c65b45751dcccecbbf8cd92aa505ef97b0c64
Mateusz Cichos <mateusz.cichos@edu.uekat.pl>
2025-02-18 23:49:14 +0100

    Add function for result of calculation

--- app.py
+++ app.py
@@ -3,6 +3,17 @@ from flask import Flask, render_template, url_for, request
 app = Flask(__name__)
 app.debug = True
 
+def calculate(a, b, operation):
+    if operation == "+":
+        return a + b
+    elif operation == "-":
+        return a - b
+    elif operation == "*":
+        return a * b
+    elif operation == "/":
+        return a / b
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -12,8 +23,11 @@ def calculator():
     if request.method == 'GET':
         return render_template('calculator.html')
     else:
-        return render_template('calculator_result.html')
+        number1 = int(request.form['number1'])
+        number2 = int(request.form['number2'])
+        operation = request.form['operation']
+        return render_template('calculator_result.html', result=calculate(number1, number2, operation))
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
